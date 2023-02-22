880fb6411d55e4313c1b1959b953af8c7acc7d91
pat <88806542+patgd@users.noreply.github.com>
2023-02-22 10:15:55 -0800

    Initial Commit

---
+++ SmithBot/SmithBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmithBotApp.swift
+//  SmithBot
+//
+//  Created by pat on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SmithBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
